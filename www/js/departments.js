861c6f0fbfbea9463ca12258c38d18d3886fe2cf
Setareh <nasihati.s@gmail.com>
2015-02-23 18:28:12 -0800

    git ignore added

--- www/js/departments.js
+++ www/js/departments.js
@@ -10,7 +10,6 @@ function setSchools(data){
     if ( localStorage['SCHOOLS']== undefined)
         localStorage['SCHOOLS'] = JSON.stringify(data);
     
-    console.log('in the set schoools and data is ' , data ) ;
     for ( var i in data){ 
         collapsible = $('<div data-role="collapsible"></div>');
         var school = $('<h2>' + data[i].SOC_SCHOOL_DESCRIPTION + '</h2>'); 
@@ -24,30 +23,18 @@ function setSchools(data){
         $("#collapsible-list").collapsibleset();
         
         getDepartments(sch_code , setDepartments , NaN , ul) ; 
-    
-        
-        
     }
 }
 
 function setDepartments(data, ul_element){
-    console.log('in the set department and data is ' , data) ;
     var school = data[0] ; 
-    console.log('school is , should be a dictionary ' , school ) ; 
-//    console.log('set dapartment seda shod ba data ' , JSON.stringify(data) , ' ul_element ' , ul_element) ; 
-    
-    if ( localStorage['SCHOOL_' + school.SOC_SCHOOL_CODE]== undefined){
+    if ( localStorage['SCHOOL_' + school.SOC_SCHOOL_CODE]== undefined)
         localStorage['SCHOOL_' + school.SOC_SCHOOL_CODE] = JSON.stringify(data);
-    }
     
-//    depts = school.SOC_DEPARTMENT_CODE ; 
-//    console.log('depts are ' , depts) ; 
     for ( var i in school.SOC_DEPARTMENT_CODE){
         var dept = school.SOC_DEPARTMENT_CODE[i]; 
-        console.log(' dept is ' , dept) ; 
         var li = $('<li><a href="index.html">'+ dept.SOC_DEPARTMENT_DESCRIPTION + '</a></li>');
         ul_element.append(li); 
-        
     }
     ul_element.listview(); 
 }
