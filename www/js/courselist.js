10ac415115476d8f3e4ef3f38cf1cad622e7abc6
Setareh <nasihati.s@gmail.com>
2015-02-25 11:00:13 -0800

    rendering courses with details but button has problem

--- www/js/courselist.js
+++ www/js/courselist.js
@@ -67,11 +67,13 @@ function renderCourses(depart){
         var min_units = mycourse.MIN_UNITS ; 
         var max_units = mycourse.MAX_UNITS ;
         var unitString  ; 
-        if ( min_units == max_units )
-            unitString = parseFloat(min_units).toString() ; 
+        if ( min_units == max_units ){
+            unitString = parseFloat(min_units).toString() ;
+            if (min_units==1) unitString+= " Unit" ; 
+            else unitString+= " Units" 
+        }
         else
-            unitString = (parseFloat(min_units).toString()) + "-" + (parseFloat(max_units).toString()) ;
-        console.log(' unit string is ' , unitString) ;
+            unitString = (parseFloat(min_units).toString()) + "-" + (parseFloat(max_units).toString()) + " Units";
         return unitString; 
     }
     console.log('in rendering coursessssssss ' , depart ) ; 
@@ -85,7 +87,7 @@ function renderCourses(depart){
             var course_heading = $('<div class="panel-heading course-heading"></div>');
             var course_title = $('<h9 class="course-title">' + course.TITLE + '</h9>');
             var label_default = $('<span class="label label-default">'+ course.SIS_COURSE_ID +'</span>');   
-            var label_info = $('<span class="label label-info">'+ getUnitString(course) +' Units</span>');
+            var label_info = $('<span class="label label-info">'+ getUnitString(course) +'</span>');
             var pull_right = $('<span class="pull-right glyphicon glyphicon-plus"></span>') ; 
             
             course_heading.append(course_title); 
@@ -93,25 +95,79 @@ function renderCourses(depart){
             course_heading.append(label_info) ; 
             course_heading.append(pull_right) ; 
             card.append(course_heading) ; 
-            $("#course-list").append(card); 
-//            class_details.append(class_description);
-//            
-//            var class_details = $('<div class="class-details"><div>'); 
-//            var class_description = $('<p class="class-description">' + course.DESCRIPTION + '</p>'); 
+             
+
+            var class_details = $('<div class="class-details"><div>'); 
+            var class_description = $('<p class="class-description">' + course.DESCRIPTION + '</p>'); 
             
             
+            var class_secions = $('<div class="class-sections"></div>') ; 
+            var panel_default = $('<div class="panel panel-default"></div>'); 
+            var panel_header = $('<div class="panel-header"><h5>Sections</h5></div>'); 
+            var panel_body = $('<div class="panel-body"></div>');
+            
+            for ( var s in course.V_SOC_SECTION){   
+                var section = course.V_SOC_SECTION[s] ;
+                console.log('section is ' , section ) ; 
+                var class_section2 = $('<div class="class-section">'); 
+                var row = $('<div class=" row"></div>');
+                var table = $('<table class="table"></table>'); 
+                var thead = $('<thead ><tr class="info"><td>Code</td><td>Type</td><td>Prof</td><td>Place</td></tr></thead>'); 
+                var tbody = $('<tbody></tbody>') ; 
+                var tr = $('<tr></tr>') ;
+                var td1 = $('<td>'+ section.SECTION+ '</td>') ;
+                var td2 = $('<td>' + section.TYPE + '</td>') ; 
+                var td3 = $('<td>' + section.INSTRUCTOR + '</td>' ) ; 
+                var td4 = $('<td>' + section.LOCATION + '</td>') ; 
+                tr.append(td1); 
+                tr.append(td2); 
+                tr.append(td3); 
+                tr.append(td4);
+                tbody.append(tr); 
+                table.append(thead); 
+                table.append(tbody);
+                
+                var table2 = $('<table class="table">');
+                var thead2 = $('<thead ><tr class="info"><td>Hours</td><td>Days</td><td>Stats</td><td>Wait List</td></tr></thead>');
+                var tbody2 = $('<tbody></tbody>') ;
+                var tr2 = $('<tr></tr>');
+                var td2_1 = $('<td>Timeeeee</td>');
+                var td2_2 = $('<td> dayssss </td>');
+                var td2_3 = $('<td>capacityyyy</td>');
+                var td2_4 = $('<td>capacityy</td>');
+                tr2.append(td2_1); 
+                tr2.append(td2_2); 
+                tr2.append(td2_3); 
+                tr2.append(td2_4); 
+                tbody2.append(tr2);
+                table2.append(thead2) ; 
+                table2.append(tbody2);
+                var add_bottun = $('<button class="btn  btn-danger">Add</button>');
+                row.append(table); 
+                row.append(table2) ; 
+                row.append(add_bottun);
+                class_section2.append(row);
+                panel_body.append(class_section2);
+            }
+            panel_default.append(panel_header);
+            panel_default.append(panel_body);
+            class_secions.append(panel_default);
+            class_details.append(class_description);
+            class_details.append(class_secions);
+            card.append(class_details) ;
+            $("#course-list").append(card);
         }
+    $('.course-card').click(function(event){
+        courseClick(this);
+    });
 }
-//    <div id="course-list">
-//       <div class="course-card panel row-fluid">
-//           <div class="panel-heading course-heading">
-//                <h9 class="course-title">Introduction To programming</h9>
-//                <span class="label label-default">CSCI-101 L</span>
-//                <span class="label label-info">3 Units</span>
-//               <span class="pull-right glyphicon glyphicon-plus"></span></div>
 
-
-
+//                                <div class="class-sections">
+//                                <div class="panel panel-default">
+//                                    <div class="panel-header">
+//                                        <h5>Sections</h5>
+//                                    </div>
+//                                    <div class="panel-body">
 
 //                            <div class="class-sections">
 //                                <div class="panel panel-default">
@@ -122,24 +178,7 @@ function renderCourses(depart){
 //                                        <div class="class-section">
 //                                            
 //                                            <div class=" row">
-//                                                <table class="table">
-//                                                    <thead >
-//                                                        <tr class="info">
-//                                                        <td>Code</td>
-//                                                        <td>Type</td>
-//                                                        <td>Prof</td>
-//                                                        <td>Place</td>
-//                                                        </tr>
-//                                                    </thead>
-//                                                    <tbody>
-//                                                        <tr >
-//                                                            <td>29901R</td>
-//                                                            <td>Lec</td>
-//                                                            <td>Redekopp</td>
-//                                                            <td>RTH115</td>
-//                                                        </tr>
-//                                                    </tbody>
-//                                                </table>
+//                                                
 //                                                <table class="table">
 //                                                    <thead >
 //                                                        <tr class="info">
@@ -165,12 +204,31 @@ function renderCourses(depart){
 //                                </div>
 //                            </div>
 
+//    <div id="course-list">
+//       <div class="course-card panel row-fluid">
+//           <div class="panel-heading course-heading">
+//                <h9 class="course-title">Introduction To programming</h9>
+//                <span class="label label-default">CSCI-101 L</span>
+//                <span class="label label-info">3 Units</span>
+//               <span class="pull-right glyphicon glyphicon-plus"></span></div>
+
+//                        <div class="class-details">
+//                            <p class="class-description">
+//                                30399-This section of CSCI 101 is 
+//                                reserved for non-majors. It is a 
+//                                terminal course in basic programming 
+//                                and does not prepare students for 
+//                                continuing on to CSCI 104.
+//                            </p>
+
+
+
 
 $(function(){
-    $('.course-card').click(function(event){
-        courseClick(this);
-    });
-        
+//    $('.course-card').click(function(event){
+//        courseClick(this);
+//    });
+//        
     console.log('heellooo??') ; 
     
     $('<div class="input-group">'+
